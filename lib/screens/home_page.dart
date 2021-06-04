7614e5cc6e06aff0ab660f9fba37020077bf5b6e
evolvingkid <fayezmohammed23@gmail.com>
2021-06-04 15:27:55 +0530

    day 2

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,10 +1,27 @@
+import 'package:bootcamp/widgets/top_var.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomepageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TopBar(
+              title: 'mohammed fayez',
+              subtitle: 'Developer',
+              color: Color(0xff0B3D2E),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Photograpghy',
+              style: TextStyle(fontSize: 22, color: Color(0xff0B3D2E)),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/top_var.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class TopBar extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final Color color;
+
+  TopBar({
+    required this.subtitle,
+    required this.title,
+    required this.color,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 44, horizontal: 23),
+      width: double.infinity,
+      color: color,
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 40,
+            backgroundImage: NetworkImage(
+                'https://images.unsplash.com/photo-1622658641558-235f26dd270b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'),
+          ),
+          const SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white),
+              ),
+              Text(
+                subtitle,
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
