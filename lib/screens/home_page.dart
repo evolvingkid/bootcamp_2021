17d4665987b394c9be0c9ec7a7f898d8c48dcabd
evolvingkid <fayezmohammed23@gmail.com>
2021-06-04 20:05:29 +0530

    day 3

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -6,20 +6,39 @@ class HomepageScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TopBar(
-              title: 'mohammed fayez',
-              subtitle: 'Developer',
-              color: Color(0xff0B3D2E),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'Photograpghy',
-              style: TextStyle(fontSize: 22, color: Color(0xff0B3D2E)),
-            )
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TopBar(
+                title: 'mohammed fayez',
+                subtitle: 'Developer',
+                color: Color(0xff0B3D2E),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Photograpghy',
+                style: TextStyle(fontSize: 22, color: Color(0xff0B3D2E)),
+              ),
+              const SizedBox(height: 10),
+              GridView.builder(
+                padding: EdgeInsets.all(10),
+                itemCount: 4,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 2),
+                itemBuilder: (ctx, index) => Container(
+                  child: Image.network(
+                    'https://images.unsplash.com/photo-1622774161048-863b17ed0d8e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
