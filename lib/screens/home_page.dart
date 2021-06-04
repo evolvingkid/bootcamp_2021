18630b37adc4d9e1c7f9c45b40103679ac83215a
evolvingkid <fayezmohammed23@gmail.com>
2021-06-04 15:26:25 +0530

    day 1

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -4,18 +4,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-            child: Container(
-                height: 100,
-                width: 100,
-                color: Colors.blueAccent,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text('data'),
-                    Text('data'),
-                  ],
-                ))));
+      body: Container(),
+    );
   }
 }
